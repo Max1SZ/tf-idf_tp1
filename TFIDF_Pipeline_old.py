33d88a3a6f31a0e80c69f1ef438c6d524c8ef674
Max1SZ <81764438+Max1SZ@users.noreply.github.com>
2025-04-22 21:23:33 -0300

    Update and rename TFIDF_PipelineOLD.py to TFIDF_Pipeline_old.py
    Esta version ya me adelante mas con ayuda de compañeros. 
    Pero en un momento llegue a este error:
    
    Traceback (most recent call last):
      File "c:\Users\estudiante\Desktop\2025\TFIDF_PipelineOLD.py", line 21, in <module>
        tokens = word_tokenize(corpus.lower())
                               ^^^^^^^^^^^^
    AttributeError: 'PlaintextCorpusReader' object has no attribute 'lower'

--- TFIDF_Pipeline_old.py
+++ TFIDF_Pipeline_old.py
@@ -13,22 +13,11 @@ nltk.download('averaged_perceptron_tagger_eng')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tabulate import tabulate #Descarga esta libreria en el cmd/bash asi luce lindo el print
 
-#corpus=PlaintextCorpusReader(".",'corpussylenguajes.txt') #obtiene texto de archivo
-corpus  =[
-"Python is an interpreted and high-level language, while CPlus is a compiled and low-level language .-",
-"JavaScript runs in web browsers, while Python is used in various applications, including data science and artificial intelligence.",
-"JavaScript is dynamically and weakly typed, while Rust is statically typed and ensures greater data security .-",
-"Python and JavaScript are interpreted languages, while Java, CPlus, and Rust require compilation before execution.",
-"JavaScript is widely used in web development, while Go is ideal for servers and cloud applications.",
-"Python is slower than CPlus and Rust due to its interpreted nature."
-"JavaScript has a strong ecosystem with Node.js for backend development, while Python is widely used in data science .-",
-"JavaScript does not require compilation, while CPlus and Rust require code compilation before execution .-",
-"Python and JavaScript have large communities and an extensive number of available libraries.",
-"Python is ideal for beginners, while Rust and CPlus are more suitable for experienced programmers."
-]
+corpus=PlaintextCorpusReader(".",'corpussylenguajes.txt') #obtiene texto de archivo
+
 #corpus=("CorpusLenguajes.txt") #obtiene texto de archivo
 # Tokenizar (separa en palabras y pasamos a minusculas)
-texto = ''.join(corpus)
+
 tokens = word_tokenize(corpus.lower())
 #wordnet
 def get_wordnet_pos(word):
@@ -37,11 +26,11 @@ def get_wordnet_pos(word):
     tag_dict = {"J":wordnet.ADJ,"N":wordnet.NOUN,"V":wordnet.VERB,"R":wordnet.ADV}
     return tag_dict.get(tag, wordnet.NOUN)
 
-#def quitarstopwords_eng(texto):
-   # ingles = stopwords.words("english")
-    #texto_limpio = [w.lower() for w in texto if w.lower() not in ingles and w not in string.punctuation
-    #                and w not in ["'s", '|', '--','-','.' "''", "``","lematizar","(",")","quitarStopwords_eng","word_tokenize"] ]
-    #return texto_limpio
+def quitarstopwords_eng(texto):
+    ingles = stopwords.words("english")
+    texto_limpio = [w.lower() for w in texto if w.lower() not in ingles and w not in string.punctuation
+                    and w not in ["'s", '|', '--','-','.' "''", "``","lematizar","(",")","quitarStopwords_eng","word_tokenize"] ]
+    return texto_limpio
 
 
 # Elimino stopwords y signos de puntuacion.
@@ -49,9 +38,9 @@ stop_words = set(stopwords.words('english')) #
 tokens_filtrados = [palabra for palabra in tokens if palabra in tokens if palabra.isalpha() and palabra not in stop_words]
 
 
-'''def lematizar(texto):
+def lematizar(texto):
     texto_lema = [lemmatizer.lemmatize(w,get_wordnet_pos(w))for w in texto]
-    return texto_lema'''
+    return texto_lema
 #iniciar lematizador
 
 lemmatizer = WordNetLemmatizer()
